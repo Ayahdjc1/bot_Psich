6c3b91420db8ad38c490af5bb5155fc7170f5372
ANnn <100271317+Ayahdjc1@users.noreply.github.com>
2025-06-01 22:43:36 +0300

    Delete start_script.sh

--- start_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source "/home/bogdan/projects_2025/telegrammbots/bot_Psich/bot/bin/activate"
-
-# Запускаем Python скрипт
-python3 "/home/bogdan/projects_2025/telegrammbots/bot_Psich/bot.py"
